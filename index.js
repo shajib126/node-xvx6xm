66234682d27b268694e1b8bacd54924824518238
shajib126 <31980836+shajib126@users.noreply.github.com>
2022-09-21 11:13:54 +0600

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// run `node index.js` in the terminal
+const http = require('http')
+const mongoClient = require('mongodb').MongoClient()
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const URL = "mongodb+srv://zubaer:abujubaer1@cluster0.iionytb.mongodb.net/test"
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200)
+  res.end("hello node js")
+})
+
+
+
+server.listen(5000,()=>console.log('server running in 5000'))
